93acd6ef984ee4bb4f802a02724a0d73ad11eabc
zekeslotsky <zekejacobs@hotmail.com>
2014-04-10 19:46:18 -0700

    add kittens

---
+++ kittens.js
@@ -0,0 +1,9 @@
+function cats() {
+var kitties=["Missy", "Minnie", "Molly", "Mo"]; 
+
+var i=0;
+while  (kitties[i]){
+	alert(kitties[i]);
+	i++; 
+}
+}
---
+++ scratch.js
@@ -0,0 +1,48 @@
+var people=["Jess", "Lorin", "Marc"];
+
+for(var i = 0; i < people.length; i++){
+	console.log(" The element at index " + i + " is:" + people[i]);
+}
+var mixed=[1, "two", "three", true];
+
+console.log("Decrementing For Loop")
+
+for(var i = mixed.length -1; i >= 0; i--){
+	console.log("The element at index " + i + " is: " + mixed[i]);
+
+
+
+var veggies=["Asparagus", "Brussel Sprouts", "Kale", "Broccoli"]; 
+
+var i=0;
+while  (veggies[i]){
+	alert(veggies[i]);
+	i++; 
+}
+
+var veggies=["Asparagus", "Brussel Sprouts", "Kale", "Broccoli"]; 
+
+var i=0;
+while  (veggies[i]){
+	console.log(veggies[i]);
+	i++; 
+}
+
+
+var veggies=["Asparagus", "Brussel Sprouts", "Kale", "Broccoli"]; 
+
+for(var i=0; veggies[i]; i++) {
+	console.log(veggies[i]);
+}
+
+
+click Me for Kitties!!
+
+onclick="neatofunction
+
+
+function neatofunction() {
+	cats=["Minnie","Mickey","Molly","Marvelous"]
+
+}
+
